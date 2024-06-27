49264f8de5f239218ec35661a71a3b222d1a6ca7
shaolinbigodudo <brunoedit234@gmail.com>
2024-06-27 12:12:29 -0300

    aula 3

--- app/fizzbuzz/fizzbuzz.py
+++ app/fizzbuzz/fizzbuzz.py
@@ -1,9 +1,9 @@
-def fizzbuzz(num:int):
-         elif num % 5 == 0 and num % 5 == 0:
-                return 'fizzbuzz'
-        if num % 3 == 0:
-                return 'fizz'
-        elif num % 5 == 0:
-                return 'buzz'
-        else:
-                return: num
+def fizzbuzz(num:int): 
+    if num % 3 == 0 and num % 5 == 0:
+        return 'fizzbuzz'
+    elif num % 3 == 0:
+        return 'fizz'
+    elif num % 5 == 0:
+        return 'buzz'
+    else:
+        return num
